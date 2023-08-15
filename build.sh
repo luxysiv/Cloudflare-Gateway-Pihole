7b1d3dbaf4a174f36eac3eb93625860acb1f9427
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-08-15 23:33:52 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+urls=(
+    https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/hosts-VN
+    https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+)
+outfile="hosts.txt"
+for url in "${urls[@]}"
+do
+    curl -s "$url" >> "$outfile"
+    echo >> "$outfile"
+done
+pip install cloudflare-gateway-adblocking
+cloudflare-gateway-adblocking --account-id "$CF_ACCOUNT_ID" --token "$CF_TOKEN" delete
+cloudflare-gateway-adblocking --account-id "$CF_ACCOUNT_ID" --token "$CF_TOKEN" upload --blocklists "$outfile"
