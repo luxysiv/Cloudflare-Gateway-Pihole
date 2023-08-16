d42aff65cc335c8af7016948d4d0b192428c6ed6
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-08-16 17:32:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#source .env
+source .env
 urls=(
-    "https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/hosts-VN"
-    "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+    https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/hosts-VN
+    https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 )
 outfile="hosts.txt"
+tempfile="temp.txt"
 for url in "${urls[@]}"
 do
-    curl -s "$url" | grep '^0\.0\.0\.0' | sort -u >> "$outfile"
+    curl -s "$url" >> "$tempfile"
+    echo >> "$tempfile"
 done
+awk '!seen[$0]++' "$tempfile" > "$outfile"
+rm "$tempfile"
 pip install cloudflare-gateway-adblocking
 cloudflare-gateway-adblocking --account-id "$CF_ACCOUNT_ID" --token "$CF_TOKEN" delete
 cloudflare-gateway-adblocking --account-id "$CF_ACCOUNT_ID" --token "$CF_TOKEN" upload --blocklists "$outfile"
