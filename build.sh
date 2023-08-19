a49772a7af50825a6ad32d178845ad1b31f59009
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-08-19 08:17:50 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,14 +6,16 @@ urls=(
     https://raw.githubusercontent.com/luxysiv/hosts/main/hosts.txt
 )
 outfile="hosts.txt"
-tempfile="temp.txt"
+#tempfile="temp.txt"
 for url in "${urls[@]}"
-do
-    curl -s "$url" >> "$tempfile"
-    echo >> "$tempfile"
+do  
+    curl -s "$url" >> "$outfile"
+    #curl -s "$url" >> "$tempfile"
+    echo >> "$outfile"
+    #echo >> "$tempfile"
 done
-grep "^0\.0\.0\.0" "$tempfile" | awk '!seen[$0]++' | sed '/0\.0\.0\.0 0\.0\.0\.0/d' > "$outfile"
-rm "$tempfile"
+#grep "^0\.0\.0\.0" "$tempfile" | awk '!seen[$0]++' | sed '/0\.0\.0\.0 0\.0\.0\.0/d' > "$outfile"
+#rm "$tempfile"
 pip install cloudflare-gateway-adblocking
 cloudflare-gateway-adblocking --account-id "$CF_ACCOUNT_ID" --token "$CF_TOKEN" delete
 cloudflare-gateway-adblocking --account-id "$CF_ACCOUNT_ID" --token "$CF_TOKEN" upload --blocklists "$outfile"
