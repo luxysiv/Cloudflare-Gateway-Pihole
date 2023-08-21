1dcb33961a0ad6c8dcd9a27a1436a44cc88804e4
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-08-21 09:09:14 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -2,7 +2,7 @@ yes | pkg upgrade && pkg install python-pip git -y
 git clone https://github.com/manhduonghn/cloudflare-gateway-adblocking
 cd cloudflare-gateway-adblocking
 pip install -r requirements.txt
-nano .env
-nano hosts-urls.ini
+command nano .env
+command nano hosts-urls.ini
 python -m src delete
 python -m src --timeout 600 upload 
