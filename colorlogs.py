e2326a02fd576030b9e6a3145f40315d1d5cb177
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-08-26 08:45:07 +0700

    Update colorlogs.py

--- colorlogs.py
+++ colorlogs.py
@@ -1,8 +1,17 @@
-yes | pkg upgrade && pkg install python-pip git -y
-git clone https://github.com/manhduonghn/cloudflare-gateway-adblocking
-cd cloudflare-gateway-adblocking
-pip install -r requirements.txt
-nano .env
-nano hosts-urls.ini
-python -m src delete
-python -m src --timeout 600 upload 
+import logging
+
+class ColoredLevelFormatter(logging.Formatter):
+    COLOR_CODE = {
+        'DEBUG':    "\x1b[36m",  
+        'INFO':     "\x1b[32m",  
+        'WARNING':  "\x1b[33m",  
+        'ERROR':    "\x1b[31m",  
+        'CRITICAL': "\x1b[31;1m" 
+    }
+
+    def format(self, record):
+        levelname = record.levelname
+        levelname_color = self.COLOR_CODE.get(levelname, "")
+        reset_color = "\x1b[0m"  
+        record.levelname = f"{levelname_color}{levelname}{reset_color}"
+        return super().format(record)
