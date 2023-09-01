36c6df1424878201abe55c148884038d12da8531
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-09-01 20:22:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -158,5 +158,5 @@ if __name__ == "__main__":
     ]
     adlist_name = "ManhDuong"
     app = App(adlist_name, adlist_urls)
-    #app.delete()
-    app.run()
+    #app.delete() # Leave script 
+    app.run()     # Use script 
