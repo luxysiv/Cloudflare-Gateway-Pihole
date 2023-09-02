bf34724b996b2665c6e112c2474c476c97eff3fe
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-09-02 07:24:52 +0700

    Đọc dạng ini mới

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from typing import List
 from src import cloudflare
 from src.utils import App
+from configparser import ConfigParser
 from src.colorlogs import ColoredLevelFormatter
 
 logging.getLogger().setLevel(logging.INFO)
@@ -14,9 +15,13 @@ logger = logging.getLogger()
 logger.addHandler(console)
 
 if __name__ == "__main__":
+    config = ConfigParser()
+    config.read("lists.ini")
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     adlist_urls = []
-    with open("lists.ini", "r") as file:
-        adlist_urls = [url.strip() for url in file if url.strip()]
+    for section in config.sections():
+            for key in config.options(section):
+                adlist_urls.append(config.get(section, key))
     adlist_name = "ManhDuong"
     app = App(adlist_name, adlist_urls)
     #app.delete() # Leave script 
