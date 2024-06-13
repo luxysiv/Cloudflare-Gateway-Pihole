557df55ac1fbe581e87e30b741ea5b50516e2518
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-13 08:22:07 +0700

    Rename and remove excess library import

--- src/cloudflare.py
+++ src/cloudflare.py
@@ -1,5 +1,4 @@
 import random
-from loguru import logger 
 from tenacity import (
     retry,
     stop_never,
@@ -8,70 +7,73 @@ from tenacity import (
 )
 from requests.exceptions import RequestException, HTTPError
 from src import (
-    error,
+    info,
     session,
-    silent_error,
     BASE_URL,
     MAX_LIST_SIZE
 )
 
-tenacity = {
+retry_config = {
     'stop': stop_never,
     'wait': wait_random_exponential(multiplier=1, max=10),
     'retry': retry_if_exception_type((HTTPError, RequestException)),
-    'after': lambda retry_state: logger.info(f"Retrying ({retry_state.attempt_number}): {retry_state.outcome.exception()}"),
-    'before_sleep': lambda retry_state: logger.info(f"Sleeping before next retry ({retry_state.attempt_number})")
+    'after': lambda retry_state: info(
+        f"Retrying ({retry_state.attempt_number}): {retry_state.outcome.exception()}"
+    ),
+    'before_sleep': lambda retry_state: info(
+        f"Sleeping before next retry ({retry_state.attempt_number})"
+    )
 }
 
-@retry(**tenacity)
+@retry(**retry_config)
 def get_current_lists():
     response = session.get(f"{BASE_URL}/lists")
     response.raise_for_status()
     return response.json()
 
-@retry(**tenacity)
+@retry(**retry_config)
 def get_current_policies():
     response = session.get(f"{BASE_URL}/rules")
     response.raise_for_status()
     return response.json()
 
-@retry(**tenacity)
+@retry(**retry_config)
 def get_list_items(list_id):
     response = session.get(f"{BASE_URL}/lists/{list_id}/items?limit={MAX_LIST_SIZE}")
     response.raise_for_status()
     return response.json()
 
-@retry(**tenacity)
+@retry(**retry_config)
 def patch_list(list_id, payload):
     response = session.patch(f"{BASE_URL}/lists/{list_id}", json=payload)
     response.raise_for_status()
     return response.json()
 
-@retry(**tenacity)
+@retry(**retry_config)
 def create_list(payload):
     response = session.post(f"{BASE_URL}/lists", json=payload)
     response.raise_for_status()
     return response.json()
 
-@retry(**tenacity)
+@retry(**retry_config)
 def create_policy(json_data):
     response = session.post(f"{BASE_URL}/rules", json=json_data)
     response.raise_for_status()
     return response.json()
 
-@retry(**tenacity)
+@retry(**retry_config)
 def update_policy(policy_id, json_data):
     response = session.put(f"{BASE_URL}/rules/{policy_id}", json=json_data)
     response.raise_for_status()
     return response.json()
 
-@retry(**tenacity)
+@retry(**retry_config)
 def delete_list(list_id):
     response = session.delete(f"{BASE_URL}/lists/{list_id}")
     response.raise_for_status()
     return response.json()
 
-@retry(**tenacity)
+@retry(**retry_config)
 def delete_policy(policy_id):
     response = session.delete(f"{BASE_URL}/rules/{policy_id}")
     response.raise_for_status()
