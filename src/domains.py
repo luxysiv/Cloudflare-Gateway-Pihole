8806d8e66570abbf48c899b677e06df881ba8665
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-13 21:41:05 +0700

    Add custom URL reading from actions variables
    This allows fork sync not to lose individual lists

--- src/domains.py
+++ src/domains.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from configparser import ConfigParser
 from src import (
@@ -7,9 +8,13 @@ from src import (
 
 class DomainConverter:
     def __init__(self):
-        self.adlist_urls = self.read_urls_from_file("./lists/adlist.ini")
-        self.whitelist_urls = self.read_urls_from_file("./lists/whitelist.ini")
-    
+        self.env_file_map = {
+            "ADLIST_URLS": "./lists/adlist.ini",
+            "WHITELIST_URLS": "./lists/whitelist.ini"
+        }
+        self.adlist_urls = self.read_urls("ADLIST_URLS")
+        self.whitelist_urls = self.read_urls("WHITELIST_URLS")
+
     def read_urls_from_file(self, filename):
         urls = []
         try:
@@ -21,28 +26,43 @@ class DomainConverter:
                         urls.append(config.get(section, key))
         except Exception:
             with open(filename, "r") as file:
-                urls = [url.strip() for url in file if not url.startswith("#") and url.strip()]
+                urls = [
+                    url.strip() for url in file if not url.startswith("#") and url.strip()
+                ]
         return urls
     
+    def read_urls_from_env(self, env_var):
+        urls = os.getenv(env_var, "")
+        return [
+            url.strip() for url in urls.split() if url.strip()
+        ]
+
+    def read_urls(self, env_var):
+        file_path = self.env_file_map[env_var]
+        urls = self.read_urls_from_file(file_path)
+        urls += self.read_urls_from_env(env_var)
+        return urls
+
     def download_file(self, url):
         r = requests.get(url, allow_redirects=True)
-        info(f"Downloaded file from {url} File size: {len(r.content)}") 
-        return r.text
-        
+        info(f"Downloaded file from {url} File size: {len(r.content)}")
+        return r.content
+
     def process_urls(self):
-        block_content = ""
-        white_content = ""
+        block_content = b""
+        white_content = b""
         for url in self.adlist_urls:
             block_content += self.download_file(url)
         for url in self.whitelist_urls:
             white_content += self.download_file(url)
             
-        with open("./lists/dynamic_blacklist.txt", "r") as black_file:
+        with open("./lists/dynamic_blacklist.txt", "rb") as black_file:
             blacklist_content = black_file.read()
             block_content += blacklist_content
         
-        with open("./lists/dynamic_whitelist.txt", "r") as white_file:
+        with open("./lists/dynamic_whitelist.txt", "rb") as white_file:
             whitelist_content = white_file.read()
             white_content += whitelist_content
-        domains = convert.convert_to_domain_list(block_content, white_content)
+        
+        domains = convert.convert_to_domain_list(block_content.decode('utf-8'), white_content.decode('utf-8'))
         return domains
