80472906637266a6bd4cbc3a67674e15769e0923
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-23 22:41:07 +0700

    Add read .env values

--- src/__init__.py
+++ src/__init__.py
@@ -4,14 +4,32 @@ import time
 import random
 import requests
 from sys import exit
-from loguru import logger
 from functools import wraps
-from dotenv import load_dotenv
+from loguru import logger
 
-# Load environment variables
-load_dotenv()
-CF_API_TOKEN = os.getenv("CF_API_TOKEN") or os.environ.get("CF_API_TOKEN")
-CF_IDENTIFIER = os.getenv("CF_IDENTIFIER") or os.environ.get("CF_IDENTIFIER")
+# Read .env
+def dot_env(file_path=".env"):
+    env_vars = {}
+    try:
+        with open(file_path) as f:
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    key, value = line.split('=', 1)
+                    key = key.strip()
+                    value = value.strip()
+                    value = re.sub(r'^["\'<]*(.*?)["\'>]*$', r'\1', value)
+                    env_vars[key] = value
+    except FileNotFoundError:
+        raise Exception(f"File {file_path} not found")
+    return env_vars
+
+# Load variables
+env_vars = dot_env()
+
+CF_API_TOKEN = os.getenv("CF_API_TOKEN") or env_vars.get("CF_API_TOKEN")
+CF_IDENTIFIER = os.getenv("CF_IDENTIFIER") or env_vars.get("CF_IDENTIFIER")
+
 if not CF_API_TOKEN or not CF_IDENTIFIER:
     raise Exception("Missing Cloudflare credentials")
 
