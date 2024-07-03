8225b6ab30271c94a6291723b27a209e81aaecb9
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-07-03 08:15:16 +0700

    warning miss CF_IDENTIFIER & CF_API_TOKEN values

--- src/__init__.py
+++ src/__init__.py
@@ -31,6 +31,8 @@ CF_API_TOKEN = os.getenv("CF_API_TOKEN") or env_vars.get("CF_API_TOKEN")
 CF_IDENTIFIER = os.getenv("CF_IDENTIFIER") or env_vars.get("CF_IDENTIFIER")
 if not CF_API_TOKEN or not CF_IDENTIFIER:
     raise Exception("Missing Cloudflare credentials")
+if CF_API_TOKEN == "your CF_API_TOKEN value" or CF_IDENTIFIER == "your CF_IDENTIFIER value":
+    raise Exception("Please provide valid values for CF_API_TOKEN and CF_IDENTIFIER")
 
 # Compile regex patterns
 replace_pattern = re.compile(
