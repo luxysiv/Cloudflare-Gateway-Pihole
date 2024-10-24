e5c38284080521be063d9d4b1966e349b90f0ece
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-10-24 09:24:37 +0700

    Use utils tools

--- src/__main__.py
+++ src/__main__.py
@@ -99,9 +99,6 @@ class CloudflareManager:
             self.cache["rules"] = []
             utils.save_cache(self.cache)
 
-def is_running_in_github_actions():
-    return os.getenv('GITHUB_ACTIONS') == 'true'
-
 def main():
     parser = argparse.ArgumentParser(description="Cloudflare Manager Script")
     parser.add_argument("action", choices=["run", "leave"], help="Choose action: run or leave")
@@ -110,7 +107,7 @@ def main():
     
     if args.action == "run":
         cloudflare_manager.update_resources()
-        if is_running_in_github_actions():
+        if utils.is_running_in_github_actions():
             utils.delete_cache()
     elif args.action == "leave":
         cloudflare_manager.delete_resources()
